3d0db0dab68cec4b8e15e7783753dab727488c07
Carla Reis <carla.jusreis@gmail.com>
2021-12-16 17:50:37 -0400

    transform route and auth to async

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -3,22 +3,26 @@ module.exports = class LoginRouter {
   constructor (authUseCase) {
     this.authUseCase = authUseCase
   }
-  route (httpRequest) {
+  async route (httpRequest) {
     try {
-      const { email, password } = httpRequest.body 
+    const { email, password } = httpRequest.body 
+
     if(!email){
       return HTTPResponse.badRequest('email')
     }
+    
     if(!password){
       return HTTPResponse.badRequest('password')
     }
 
-    const accessToken = this.authUseCase.auth(email, password)
+    const accessToken = await this.authUseCase.auth(email, password)
+    
     if(!accessToken){
       return HTTPResponse.unauthorizedError()
     }
 
     return HTTPResponse.ok({accessToken})
+
     } catch (error) {
       //console.error(error)
       return HTTPResponse.serverError()
